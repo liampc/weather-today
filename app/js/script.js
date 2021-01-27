f3ce3e8b5d4c67896c06fdeaca22662c9e6c1a18
liampc <kathliampcarlos@gmail.com>
2021-01-27 16:13:22 +0800

    add getCurrentTime func

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
-console.log("Hello world")
+const time = document.querySelector("#time")
+const ampm = document.querySelector("#ampm")
 
 async function getWeather(){
     const response = await fetch("http://dataservice.accuweather.com/forecasts/v1/daily/1day/264884?apikey=Vb1TkVOlVB5OhYJE6kzYMXXYtBXjUeKj")
@@ -6,3 +7,22 @@ async function getWeather(){
         console.log(response)
     })
 }
+
+function getCurrentTime(){
+    let current = new Date()
+    
+
+    let hour = current.getHours() 
+    let getampm = hour >= 12 ? 'PM' : 'AM'
+    hour = hour >= 12 ? hour % 12 : hour
+
+    let mins = current.getMinutes() 
+    mins = mins < 10 ? "0" + mins : mins
+
+    
+    time.innerHTML = `${hour} : ${mins}`
+    ampm.innerHTML = getampm
+}
+
+let getTime = setInterval(() => getCurrentTime(), 1000);
+
