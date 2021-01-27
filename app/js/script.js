3a1bac786585f4dc4124ce27bcbae653f2bc8c72
liampc <kathliampcarlos@gmail.com>
2021-01-27 15:45:30 +0800

    fetch api:

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,8 @@
 console.log("Hello world")
+
+async function getWeather(){
+    const response = await fetch("http://dataservice.accuweather.com/forecasts/v1/daily/1day/264884?apikey=Vb1TkVOlVB5OhYJE6kzYMXXYtBXjUeKj")
+    response.json().then(function(response){
+        console.log(response)
+    })
+}
